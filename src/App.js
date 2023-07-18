44911522d74b7c712cd0c88a4a0875b6824d1bdf
TeddyS26 <tsann010@uottawa.ca>
2023-07-18 16:13:17 -0400

    fix link t 404

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Plans from './components/Plans';
@@ -14,16 +14,16 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <Routes>
-          <Route path="/" element={<WithNavbar component={Home} />} />
-          <Route path="/plans" element={<WithNavbar component={Plans} />} />
-          <Route path="/movies-shows" element={<WithNavbar component={MoviesShows} />} />
-          <Route path="/subscribe" element={<WithNavbar component={Subscribe} />} />
-          <Route path="/login" element={<WithNavbar component={Login} />} />
-          <Route path="/signup" element={<WithNavbar component={SignUp} />} />
-          <Route path="/main" element={<Main />} />
-          <Route path="/*" element={<NotFoundPage />} />
-        </Routes>
+      <Routes>
+        <Route path="/" element={<WithNavbar component={Home} />} />
+        <Route path="/plans" element={<WithNavbar component={Plans} />} />
+        <Route path="/movies-shows" element={<WithNavbar component={MoviesShows} />} />
+        <Route path="/subscribe" element={<WithNavbar component={Subscribe} />} />
+        <Route path="/login" element={<WithNavbar component={Login} />} />
+        <Route path="/signup" element={<WithNavbar component={SignUp} />} />
+        <Route path="/main" element={<Main />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
       </div>
     </Router>
   );
